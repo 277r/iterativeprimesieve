3c57c7ed4641374ec21f4262bef3adbe0b407589
rs <rs@debian.home>
2026-04-14 22:33:57 +0200

    improved readability

--- prime.cpp
+++ prime.cpp
@@ -1,55 +1,70 @@
+// Author: RS
+// To Do:   
+//          remove modulo check below "Duurt Lang!"
+// Done:
+//          change SLS to be entered in program
+//          Change list to only contain counters (use sets of index matched maxes and counters?) to improve memory, should have processor perfomance effect as well
+// Optional:
+//          Change SLS to variable list size with increment size steps, to automatically utilize more memory if more needed, would require realloc
 #include <iostream>
 #include <cmath>
 #include <x86_64-linux-gnu/gmp.h>
-#define SLS 20000
 #define LOGLEVEL 1
+
 
 int main(int argc, char* argv[]){
 
-    if (argc < 2){
+    if (argc < 3){
         return -1;
     }
     
-    int x = std::stoi(argv[1]);  
+    unsigned long long x = std::stoull(argv[1]);  
+    unsigned long long SLS = std::stoull(argv[2]);  
     if ((x & 1) == 0 ) {
         std::cout << "ispriment";
         return -1;
     }
-        // create list, set zero  
-    bool *numArray_en = new bool[SLS];
-    int *numArray = new int[SLS];
-    for (int i = 0; i < SLS; i++){
-        numArray[i] = 0;
-        // zero is TRUE, 1 is FALSE, this could optimize since 'jz' faster than 'jnz'
-        numArray_en[i] = 1;
-    }
-    numArray_en[3] = 0;
+    int numlistIndex = 0;
+    // create list, set zero  
+    unsigned long long *numArray_max = new unsigned long long[SLS];
+    unsigned long long *numArray = new unsigned long long[SLS];
 
-    int ROOT = std::sqrt(x) + 1;
+
+
+    for (unsigned long long i = 0; i < SLS; i++){
+        numArray[i] = 0;
+        numArray_max[i] = 0;
+    }
+
+    unsigned long long ROOT = std::sqrt(x) + 1;
+
+
      // zero is TRUE, 1 is FALSE, this could optimize since 'jz' faster than 'jnz'
     bool divisor = 1;
 
 
-    for (int i = 3; i < ROOT; i += 2){
+    for (unsigned long long i = 3; i < ROOT; i += 2){
         divisor = 1;
         
         // iterate through list and increment each value that needs incrementing
-        for (int j = 3; j < SLS; j++){
-            if (numArray_en[j] == 0){
+        for (unsigned long long j = 0; j < numlistIndex; j++){
                 numArray[j]++;
                 // if divisor equals incremention, signal STOP, meaning non-prime
-                if (numArray[j] == j){
+                if (numArray[j] == numArray_max[j]){
                     divisor = 0;
                     numArray[j] = 0;
                 }
-            }
+                if (numArray[j] == x){
+                    std::cout << "ISPRIME" << "\n";
+                    return;
+                }
 
         }
 
 
 
 #if LOGLEVEL > 4
-        for (int k = 0; k < SLS; k++){
+        for (unsigned long long k = 0; k < SLS; k++){
             std::cout << numArray[k] << ", ";
 
         }
@@ -61,24 +76,21 @@ int main(int argc, char* argv[]){
         }
 
 
-
+        
         // DUURT LANG!!
-        int j = x % i;
-        if (j == 0){
+        unsigned long long rem = x % i;
+        if (rem == 0){
             std::cout << "ISPRIMENT\n";
             return -1;
         }
-        if (j != 0){
-            numArray_en[i] = 0;
-            numArray[i] = 0;
+        if (rem != 0){
+            //std::cout << "new prime found:" << i << "\n";
+            numArray[numlistIndex] = 0;
+            numArray_max[numlistIndex] = i;
+            numlistIndex++;
             continue;
         }
-
         
-
-
-        
-
     }
     
     std:: cout << "NUM ISPRIME\n";
